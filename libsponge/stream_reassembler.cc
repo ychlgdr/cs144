1967199a147fc3a05e53e1cfe215dd4a8c4d42d2
ychlgdr <905779660@qq.com>
2022-09-13 07:41:08 -0700

    bug(stream_reassembler):bad code need to fix

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -8,19 +8,105 @@
 // You will need to add private members to the class declaration in `stream_reassembler.hh`
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity)
+    : _output(capacity), _capacity(capacity), _unassembled_str(), _unassembled_interval() {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    size_t len = data.length();
+    size_t next = index + len;
+    // const string &s = data;
+    size_t begin = index;
+    size_t end = next - 1;
+    // if (index < _next_assembled) {
+    //     const string &a = s.substr(_next_assembled - index);
+    //     begin = _next_assembled;
+    // }
+    // size_t begin = index;
+    // size_t end = next - 1;
+
+    // check if out of eof range or multily eof
+    if (_eof && (next > _eof_index || eof))
+        return;
+
+    // check if out of capacity
+    size_t m = max_unassembled();
+    if (next > m) {
+        // const string &s = data.substr(0, m - index);
+        // push_substring(s, index, eof);
+        return;
+    }
+
+    // check if intersect
+    // if (index < _next_assembled)
+    //     return;
+    // for (auto &p : _unassembled_interval) {
+    //     if (max(p.first, begin) < min(p.second, end))
+    //         return;
+    // }
+
+    if (eof) {
+        _eof = true;
+        _eof_index = next;
+    }
+
+    auto cmp = [](pair<size_t, size_t> a, pair<size_t, size_t> b) -> bool {
+        if (a.first < b.first)
+            return true;
+        if (a.first == b.first)
+            return a.second <= b.second;
+    };
+
+    for (int i = 0; i < _unassembled_interval.size(); i++) {
+        if (i + 1 == _unassembled_interval.size()) {
+            _unassembled_interval.emplace_back(make_pair(begin, end));
+            break;
+        }
+        auto b = _unassembled_interval[i];
+        auto n = _unassembled_interval[i + 1];
+        //if pair smaller than before larger than next,then insert it
+        if (cmp(b, {begin, end}) && cmp({begin, end}, n)) {
+            _unassembled_interval.insert(_unassembled_interval.begin() + i + 1, {begin, end});
+            break;
+        }
+    }
+    if (_unassembled_interval.empty()) {
+        _unassembled_interval.emplace_back(make_pair(begin, end));
+    }
+    // _unassembled_interval.emplace(begin, end);
+    _unassembled += len;
+    _unassembled_str.insert({begin, data});
+
+    for(auto & p : _unassembled_interval){
+        if(p.first <= _next_assembled){
+            const string &str = _unassembled_str.at(p.first);
+            _unassembled_interval.erase(_next_assembled);
+            
+        }
+    }
+
+    // while (_unassembled_str.find(_next_assembled) != _unassembled_str.end()) {
+    //     const string &str = _unassembled_str.at(_next_assembled);
+
+    //     _unassembled_str.erase(_next_assembled);
+    //     _unassembled_interval.erase(_next_assembled);
+    //     _unassembled -= str.length();
+    //     _next_assembled += str.length();
+    //     // size_t e = _next_assembled + str.length() - 1;
+
+    //     _output.write(str);
+
+    //     if (_eof && _next_assembled == _eof_index)
+    //         _output.end_input();
+    // }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return _unassembled; }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return _output.buffer_empty() && _unassembled == 0; }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,15 +5,27 @@
 
 #include <cstdint>
 #include <string>
-
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+// using namespace std;
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
 
-    ByteStream _output;  //!< The reassembled in-order byte stream
-    size_t _capacity;    //!< The maximum number of bytes
+    ByteStream _output;      //!< The reassembled in-order byte stream
+    size_t _capacity;        //!< The maximum number of bytes
+    size_t _unassembled{0};  //!< The unassembled number
+    size_t _eof_index{0};
+    size_t _next_assembled{0};  //!< The next position being assembled
+    bool _eof{};
+    std::unordered_map<size_t, const std::string &> _unassembled_str;  // key is index,value is string
+    // std::unordered_map<size_t, size_t> _unassembled_interval;
+    std::vector<pair<size_t,size_t> > _unassembled_interval;
+    // return max unassembled position restrict by "_capacity"
+    size_t max_unassembled() { return _capacity - _output.buffer_size() + _next_assembled; }
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
